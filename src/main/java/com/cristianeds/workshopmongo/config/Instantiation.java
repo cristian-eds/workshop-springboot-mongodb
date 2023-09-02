3032353183560630f02637e55d4a8ff9635fe28e
Cristian Eduardo <cristian.santos0308@gmail.com>
2023-09-02 15:14:31 -0300

    Post entity

--- src/main/java/com/cristianeds/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cristianeds/workshopmongo/config/Instantiation.java
@@ -1,19 +1,27 @@
 package com.cristianeds.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.cristianeds.workshopmongo.domain.Post;
 import com.cristianeds.workshopmongo.domain.User;
+import com.cristianeds.workshopmongo.repository.PostRepository;
 import com.cristianeds.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -22,7 +30,13 @@ public class Instantiation implements CommandLineRunner{
 		User joaoUser = new User(null, "Joao Carlos", "joao.carlos@gmail.com");
 		User taldoUser = new User(null, "Taldo Jr", "taldo.jr@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("09/09/2023"), "Partiu viagem", "Vou viajar para SP", juliaUser);
+		Post post2 = new Post(null, sdf.parse("10/09/2023"), "Bom dia", "Cheguei em SP", juliaUser);
+		
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		repository.saveAll(Arrays.asList(juliaUser,joaoUser,taldoUser));
+		
+		
 		
 	}
 	
