5a08c51af49e74fdf83bef50aecaf62f2b178e0a
Cristian Eduardo <cristian.santos0308@gmail.com>
2023-09-06 21:01:14 -0300

    Query full search - , ,

--- src/main/java/com/cristianeds/workshopmongo/service/PostService.java
+++ src/main/java/com/cristianeds/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.cristianeds.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +21,14 @@ public class PostService {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public List<Post> findByTitle(String text) {
 		return postRepository.searchTitle(text);
 	}
-	
-	
-	
-	
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
+
 }
